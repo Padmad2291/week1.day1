09877023850083005e85fa1dfe2b04d9aa115f57
Padmad2291 <109227245+Padmad2291@users.noreply.github.com>
2022-07-13 21:53:46 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		 int firstNum=0;
+		 int secNum=1;
+		 int sum;
+		 
+		 for(int i=0;i<11;i++)
+		 {
+			 System.out.println(firstNum);
+			 sum=firstNum+secNum;
+			 firstNum=secNum;
+			 secNum=sum;
+		 }
+
+	}
+
+}
